6b9f8e6b150508c61549ff9c9954b838326cb011
moonpiderman <lotsofchuck@gmail.com>
2021-02-18 18:53:41 +0900

    Add models(seminar, userseminar, part_user, inst_user)

--- waffle_backend/seminar/models.py
+++ waffle_backend/seminar/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Seminar(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.PositiveSmallIntegerField(blank=True)
+    count = models.PositiveSmallIntegerField(blank=True)
+    # time =
+    online = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class UserSeminar(models.Model):
+    user = models.ForeignKey(User, null=True, related_name='user_seminar', on_delete=models.SET_NULL)
+    seminar = models.ForeignKey(Seminar, null=True, related_name='user_seminar', on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- waffle_backend/user/models.py
+++ waffle_backend/user/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class ParticipnatProfile(models.Model):
+    user = models.OneToOneField(User, null=True, related_name='participant', on_delete=models.SET_NULL)
+    university = models.CharField(max_length=50, blank=True)
+    accepted = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class InstructorProfile(models.Model):
+    user = models.OneToOneField(User, null=True, related_name='instructor', on_delete=models.SET_NULL)
+    company = models.CharField(max_length=50, blank=True)
+    year = models.PositiveSmallIntegerField(default=0, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
